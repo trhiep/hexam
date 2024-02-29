9e0ac5dfefbe811b8bba2ed78eca19a6487ae928
trhiep <hieptran.pa@gmail.com>
2024-02-29 20:40:46 +0700

    Project init

---
+++ heducation/src/main/java/com/hieptv/heducation/HeducationApplication.java
@@ -0,0 +1,13 @@
+package com.hieptv.heducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeducationApplication.class, args);
+    }
+
+}
---
+++ heducation/src/test/java/com/hieptv/heducation/HeducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieptv.heducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
